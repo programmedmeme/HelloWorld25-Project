93aea305935fb8dd4532111fc8f01b3f30711309
Alex Zhou <alexzhou134@gmail.com>
2025-09-20 17:42:19 -0400

    main js

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,22 @@
 function drinkRecommendation() {
     const mood = document.getElementById("mood").value.toLowerCase();
-    let drink="";
+    const data = {
+        mood: mood
+    }
+
+    fetch("/api/recommend", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })  
+    .then(response => response.text()) // Get the response from the server as plain text
+    .then(responseText => {
+        // Display the response message on the page
+        document.getElementById('').innerText = responseText;
+    })
+    .catch(error => console.error('Error:', error));
 }
+
+
