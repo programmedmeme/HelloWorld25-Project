4a2e8bdc24e17cf5fe07e2cfa0a1f9278f728c00
Matthew <dasdasd>
2025-09-20 15:25:31 -0400

    delete example, test genai

---
+++ backend/app/src/main/java/org/example/GenerateTextFromTextInput.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+
+public class GenerateTextFromTextInput {
+    public static void main(String[] args) {
+        Client client = Client.builder()
+            .apiKey("AIzaSyAPjd3gJCTTdFrLwMZcD7R56n1ku2svOho")
+            .build();
+
+        GenerateContentResponse response = client.models.generateContent(
+            "gemini-2.5-flash",
+            "Explain how AI works in a few words",
+            null
+        );
+
+        System.out.println(response.text());
+    }
+}
